e7fa3899146008a0416d5b5144aed8d8b6bf3b49
EmmaMQJin <mqjin@seas.upenn.edu>
2024-04-16 16:44:46 -0400

    refactored code

--- playground/generate_locations_HITL.py
+++ playground/generate_locations_HITL.py
@@ -1,35 +1,27 @@
-from generate_locations import *
+from generate_locations_utils import *
 import tempfile
 import subprocess
+import os
 
 def open_vim_with_string(text):
-    # Create a temporary file
     with tempfile.NamedTemporaryFile(mode='w+', suffix='.txt', delete=False) as temp_file:
-        # Write the string to the temporary file
         temp_file.write(text)
-
-    # Open the temporary file in Vim
     subprocess.run(['vim', temp_file.name])
 
-    # Read the edited string from the temporary file
     with open(temp_file.name, 'r') as edited_file:
         edited_text = edited_file.read()
-
-    # Delete the temporary file
-    # Remove the temporary file
-    import os
     os.unlink(temp_file.name)
 
     return edited_text
 
 
-def generate_all_locations(desc, examples):
+def generate_central_loc_HITL(desc, example, central_loc_shots):
     # generate central location with user input
     central_loc_desc = input("Do you have any thoughts on what the central location is like?\n")
     all_descs = desc
     if central_loc_desc:
         all_descs += "\nAbout the central location of the game: " + central_loc_desc
-    loc_center = generate_new_location(all_descs, examples)
+    loc_center = generate_new_location(all_descs, example, central_loc_shots)
     
     # get feedback from user about central location
     print("OK, here is the generated central location:")
@@ -44,24 +36,55 @@ def generate_all_locations(desc, examples):
 
     dict_to_json_file(loc_center, "test_generations/init_location.json")
 
+def main():
+    story_cyberpunk = read_file_to_str("data/story-cyberpunk.txt")
+    story_insidetemple = read_file_to_str("data/story-insidetemple.txt")
+    story_lake = read_file_to_str("data/story-lake.txt")
+
+    central_loc_lake_obj = read_json_examples(
+        "data/few-shot-examples/central-loc-lake.json")
+    neib_locs_lake_5_list = read_json_examples(
+        "data/few-shot-examples/neighb-locs-lake-5.json")
+
+    central_loc_insidetemple_obj = read_json_examples(
+        "data/few-shot-examples/central-loc-insidetemple.json")
+    neib_locs_insidetemple_3_list = read_json_examples(
+        "data/few-shot-examples/neighb-locs-insidetemple-3.json")
+    
+    hall_of_goddess_obj = read_json_examples("data/few-shot-examples/hall-of-goddess.json")
+    royal_tomb_obj = read_json_examples("data/few-shot-examples/royal-tomb.json")
+
+        # few-shot for central location format
+    central_loc_shot_1 = create_new_location_shot(story_insidetemple, central_loc_insidetemple_obj)
+    central_loc_shot_2 = create_new_location_shot(story_lake, central_loc_lake_obj)
+    central_loc_shots = central_loc_shot_1 + central_loc_shot_2
+
+    # few-shot for neighboring locations
+    neib_locs_shot_1 = create_neib_locs_shot(
+        central_loc_insidetemple_obj, story_insidetemple, 1, neib_locs_insidetemple_3_list[:1])
+    neib_locs_shot_2 = create_neib_locs_shot(
+        central_loc_insidetemple_obj, story_insidetemple, 3, neib_locs_insidetemple_3_list)
+    neib_locs_shot_3 = create_neib_locs_shot(
+        central_loc_lake_obj, story_lake, 5, neib_locs_lake_5_list)
+    neib_locs_shots = neib_locs_shot_1 + neib_locs_shot_2 + neib_locs_shot_3
+
+    # few-shot for connections
+    hall_tomb_connection = [{"direction": "down",
+                             "travel description": "Descending the stairs from the Hall of the Goddess, you move towards the Royal Tomb, the air growing cooler and heavier with the weight of centuries."},
+                            {"direction": "up",
+                             "travel description": "Ascending the stairs from the depths of the Royal Tomb, you journey back towards the Hall of the Goddess. With each step upwards, the air becomes lighter and warmer, shedding the cool, heavy presence of ancient history. The atmosphere subtly shifts, as if leaving behind the echoes of the past to embrace the divine serenity of the Goddess's hall."}]
+    connections_shot_1 = create_connections_shot(hall_of_goddess_obj, royal_tomb_obj, hall_tomb_connection)
+    connections_shots = connections_shot_1
+
+    print("Welcome to WorldWeaver!")
+
+    # desc = input("Please give a description of the type of game you want to build.\n")
+
+    choice = input("Would you like to start with generating locations or characters for your game?\n")
+
+    if choice.lower() == "location":
+        generate_central_loc_HITL(story_cyberpunk, neib_locs_insidetemple_3_list[0], central_loc_shots)
 
 
-
-
-
-
-story_desc = read_file_to_str("data/story-cyberpunk.txt")
-location_init_examples = read_json_examples("data/locations-name-desc-4.json")
-location_init_story_insidetemple = read_file_to_str("data/story-insidetemple.txt")
-location_init_story_lake = read_file_to_str("data/story-lake.txt")
-location_neib_prompt_example = read_json_examples("data/location-name-desc-1.json")
-location_neibs_example_lake = read_json_examples("data/locations.name-desc-lake.json")
-
-print("Welcome to WorldWeaver!")
-
-# desc = input("Please give a description of the type of game you want to build.\n")
-
-choice = input("Would you like to start with generating locations or characters for your game?\n")
-
-if choice.lower() == "location":
-    generate_all_locations(story_desc, location_init_examples)
+if __name__ == "__main__":
+    main()
