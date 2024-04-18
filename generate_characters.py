961ae9134dc441480989cf19505fb9a2275fba9d
shriyaRamakrishnan <157773045+shriyaRamakrishnan@users.noreply.github.com>
2024-04-18 15:06:04 -0400

    updated character code

--- generate_characters.py
+++ generate_characters.py
@@ -37,7 +37,7 @@ directory = ''  # Adjust the path to the directory of your JSON files
 output_filename = 'extracted_characters.json'  # The filename for the output JSON
 
 # Extract character data
-extracted_characters = extract_characters(directory)
+# extracted_characters = extract_characters(directory)
 
 # Save the extracted data to a new JSON file in the same directory as this script
 #save_json(extracted_characters, output_filename)
@@ -46,22 +46,23 @@ extracted_characters = extract_characters(directory)
 
 ####################
 #few shot GPT-4
-def promptGPT(directory):
+def promptGPT(story, directory):
     if 'HELICONE_API_KEY' not in os.environ:
-        os.environ['HELICONE_API_KEY'] = ''
+        os.environ['HELICONE_API_KEY'] = 'sk-helicone-cp-spr2h6y-tdie7wq-whq5thq-oq33ugy'
 
 
     client = OpenAI(base_url="https://oai.hconeai.com/v1", api_key=os.environ['HELICONE_API_KEY'])
 
-    chars = {}
+    output = {}
 
-    with open("extracted_characters.json", 'r') as file:
-        chars = json.load(file)
+    with open("/Users/shriyaram/Documents/Spring24/txtgen/CIS-7000/Project/playground/data/few-shot-examples/example-character.json", 'r') as file:
+        output = json.load(file)
  
-    prompt = "Based on the given example create a new character with all the key value pairs. Do not generate any character more than once."
+    prompt = f"You are a helpful character generator for building a text adventure game. Given the background story of the game from the user, generate one character in a JSON, formatted like the examples below:"
     messages = [
         {'role': 'system', 'content': prompt},
-        {'role': 'user', 'content': f"{json.dumps(chars[:4], indent=4)}"}
+        {'role': 'user', 'content': "Background story: f{story} Location to generate neighboring locations for:".format(story=story)},
+        {"role": "assistant", "content": json.dumps(output)}
     ]
     response = client.chat.completions.create(
         model='gpt-4',
@@ -73,14 +74,13 @@ def promptGPT(directory):
         presence_penalty=0
     )
     gpt_response = response.choices[0].message.content
-    filename = "analysis.json"
-    with open(filename, 'r+') as file:
-        file.seek(0, 2)  # Move the file pointer to the end of the file
-        if file.tell()!=0:
-            file.seek(file.tell() - 1, 0)  # Move the file pointer to just before the last character
-            file.write(',\n' + json.dumps(json.loads(gpt_response), indent=4) + '\n]')
-        else:
-            file.seek(0, 0)
-            file.write(json.dumps(json.loads(gpt_response), indent=4) + '\n]')
-
-promptGPT(directory)
+    # filename = "analysis.json"
+    # with open(filename, 'r+') as file:
+    #     file.seek(0, 2)  # Move the file pointer to the end of the file
+    #     if file.tell()!=0:
+    #         file.seek(file.tell() - 1, 0)  # Move the file pointer to just before the last character
+    #         file.write(',\n' + json.dumps(json.loads(gpt_response), indent=4) + '\n]')
+    #     else:
+    #         file.seek(0, 0)
+    #         file.write(json.dumps(json.loads(gpt_response), indent=4) + '\n]')
+    return json.loads(gpt_response)
